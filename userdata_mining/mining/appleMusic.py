225e6f7525a696236fd61a386d3efaae18602ac9
mngarity <mngarity@ncsu.edu>
2021-04-07 20:08:45 -0400

    Added mining for apple music, tv shows, and app store activity

---
+++ userdata_mining/mining/appleMusic.py
@@ -0,0 +1,26 @@
+import os
+import json
+import pandas as pd
+
+def parse_tv_data(user, data_path='.'):
+    """
+    Mines a user's app store clicks. While it is possible to mine the user's
+    (or, the people the user is speaking to) data selectively, we do not
+    do that distinction here.
+
+    :param {str} user - The user directory.
+    :param {str} data_path - Path to the data/ directory, NOT ending in a /.
+    :return {list} user messages
+    """
+    # Does the directory exist?
+    path = f'{data_path}/data/{user}/Apple Music Play Activity.xlsx'
+    if not os.path.exists(path):
+        return []
+
+    data = pd.read_excel(path, sheet_name=0, header=1)
+
+    item = data['Artist Name'].dropna()
+    pageDescription = data['Content Name'].dropna()
+    return item, pageDescription
+
+print(parse_tv_data('madison', '../..'))
---
+++ userdata_mining/mining/tvShows.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+def parse_tv_data(user, data_path='.'):
+    """
+    Mines a user's  TV shows. While it is possible to mine the user's
+    (or, the people the user is speaking to) data selectively, we do not
+    do that distinction here.
+
+    :param {str} user - The user directory.
+    :param {str} data_path - Path to the data/ directory, NOT ending in a /.
+    :return {list} user messages
+    """
+    # Does the directory exist?
+    path = f'{data_path}/data/{user}/TV App Favorites and Activity.json'
+    print(path)
+    if not os.path.exists(path):
+        return []
+
+    with open(path, 'r') as f:
+        data = json.load(f)
+
+    # The conversation key only has metadata, skip it.
+    shows = data['events']
+
+    tvShows = [p['event_interpretation']['human_readable_media_description'] for p in shows]
+    return tvShows
+
